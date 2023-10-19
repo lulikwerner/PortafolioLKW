8117d39a2a9f5a3518ad360a69f298c16a4b5248
Lucila Werner <lulikwerner@gmail.com>
2023-10-19 12:36:05 -0400

    navbar ok

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -17,11 +17,10 @@ const Navbar = () => {
                     <button onClick={() => scrollToSection('section1')} className="Option-Nav">Home</button>
                     <button onClick={() => scrollToSection('section2')} className="Option-Nav">About</button>
                     <button onClick={() => scrollToSection('section3')} className="Option-Nav">Portafolio</button>
-                    <button onClick={() => scrollToSection('section4')} className="Option-Nav">Contact</button>
+                    <button onClick={() => scrollToSection('section4')} className="Option-Nav">Contact</button>   
                     <label htmlFor="check" className="close-menu"><i className="fas fa-times"></i></label>             
                     <label htmlFor="check" className="open-menu"><i className="fas fa-bars"></i></label>
                 </ul>
-
             </nav>
         </header>
     );
