26f42c23ec5e0dbd4f748722b90503d86a40d432
Lucila Werner <lulikwerner@gmail.com>
2023-11-01 12:52:53 -0400

    responsive nav

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,26 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
 
 const Navbar = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     const scrollToSection = (sectionId) => {
         const section = document.querySelector(`#${sectionId}`);
         if (section) {
             section.scrollIntoView({ behavior: 'smooth', block: 'start' });
+            setIsMenuOpen(false); 
         }
     };
 
     return (
         <header>
-            <nav className="Nav">
+            <nav className={`Nav ${isMenuOpen ? 'open' : ''}`}>
                 <ul>
-                <li>LUCILA KERSTIN <span>WERNER</span></li>
+                    <li>LUCILA KERSTIN <span>WERNER</span></li>
                     <button onClick={() => scrollToSection('section1')} className="Option-Nav">Home</button>
                     <button onClick={() => scrollToSection('section2')} className="Option-Nav">About</button>
-                    <button onClick={() => scrollToSection('section3')} className="Option-Nav">Portafolio</button>
-                    <button onClick={() => scrollToSection('section4')} className="Option-Nav">Contact</button>   
-                    <label htmlFor="check" className="close-menu"><i className="fas fa-times"></i></label>             
-                    <label htmlFor="check" className="open-menu"><i className="fas fa-bars"></i></label>
+                    <button onClick={() => scrollToSection('section3')} className="Option-Nav">Portfolio</button>
+                    <button onClick={() => scrollToSection('section4')} className="Option-Nav">Contact</button>
                 </ul>
+                <div className={`menu-toggle ${isMenuOpen ? 'open' : ''}`} onClick={toggleMenu}>
+                    <div className="bar"></div>
+                    <div className="bar"></div>
+                    <div className="bar"></div>
+                </div>
             </nav>
         </header>
     );
