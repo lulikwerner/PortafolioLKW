8503141e2644d188e2ac0a5bc0b59be38cb3336c
Lucila Werner <lulikwerner@gmail.com>
2023-12-14 19:26:05 -0500

    form

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,13 +1,38 @@
-
+import React from 'react';
+import './Contact.css';
 
 const Contact = () => {
-    return (
-        <header>
-          <nav>
-            Soy el Contact
-          </nav>
-        </header>
-      );
-    };
-    
-    export default Contact;
+  return (
+    <header className="header-form">
+      <meta name="keywords" content="Contact, Work, Quote, Hire"></meta>
+      <nav className="form_format">
+        <h1 className="title-form">Get in Touch</h1>
+        <p className="description-form">
+          For business inquiries and collaborations, please contact me below. I
+          will get back to you promptly. Please allow me 1-2 business days for
+          my response—you might even receive a response the same day. <br></br>I am
+          looking forward to hearing from you!
+        </p>
+        <form id="my-form" className="my-form" action="/your-form-endpoint" method="post">
+          <div className="box1">
+          <input type="text" name="email" className="box" id="emailInput" placeholder="Email" />
+          </div>
+          <div className="box2">
+          <input type="text" name="name" className="box" id="nameInput" placeholder="Name" />
+          </div>
+          <div className="box3">
+           <input name="text" className="box box3" id="messageInput" placeholder="Message" />
+</div>
+          <div className="wrap4">
+            <button type="submit" className="btn-sumbit" >
+              Submit
+            </button>
+          </div>
+        </form>
+      </nav>
+    </header>
+  );
+};
+
+
+export default Contact;
