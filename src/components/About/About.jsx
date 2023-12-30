1fa5870d2fe73372fb70ec8873c573f6b62bf327
Lucila Werner <lulikwerner@gmail.com>
2023-12-29 22:56:08 -0500

    css home

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,24 +3,25 @@ import { Link } from 'react-router-dom';
 import './About.css';
 
 const About = () => {
-  const scrollToSection = (sectionId) => {
-    const section = document.querySelector(`#${sectionId}`);
-    if (section) {
-        section.scrollIntoView({ behavior: 'smooth', block: 'start' });
-        setIsMenuOpen(false); 
-    }
-};
+    const scrollToSection = (sectionId) => {
+        const section = document.querySelector(`#${sectionId}`);
+        if (section) {
+            section.scrollIntoView({ behavior: 'smooth', block: 'start' });
+            setIsMenuOpen(false);
+        }
+    };
+
     return (
         <main id="section1">
             <section className="contenta">
                 <div className="img-about">
-                    <img src="" />
+                    <img className="img-link-abt" src="../src/assets/img/office.png" />
                 </div>
                 <div className="about-abt">
                     <div className="about">
                         <h1 className="titlea">Let's Build something together</h1>
                         <p className="descriptiona">
-                            I craft user interfaces using modern frontend 
+                            I craft user interfaces using modern frontend
                             web technologies with back end development skills based in Python and Javascript.
                         </p>
                         <button onClick={() => scrollToSection('section3')} className="about-btn">My Projects</button>
--- src/utils/gFetch.js
+++ src/utils/gFetch.js
@@ -3,7 +3,8 @@ let projects = [
     { id: 2, title: "BMI", img:"../src/assets/img/scale.png", description:"Javascript - CSS - HTML", link:"https://bodymass-calculator.netlify.app/" },
     { id: 3, title: "MIA JEWELERS", img:"../src/assets/img/jewelry2.png", description:"Javascript - React - CSS - HTML", link:"https://miajewelers.netlify.app/" },
     { id: 4, title: "OYL FASHION", img:"../src/assets/img/OYL.png", description:"Javascript - React - CSS - HTML - Figma", link:"https://oyl-indumentaria.netlify.app/"},
-    { id: 5, title: "FULLSTACK", img:"../src/assets/img/ecommerce.png", description:"Javascript - Mongoose - Postman - NPM - Token - Cookies - CSS - HTML ", link:"https://backend-commerce-dev.onrender.com/"}
+    { id: 5, title: "FULLSTACK", img:"../src/assets/img/ecommerce.png", description:"Javascript - Mongoose - Postman - NPM - Token - Cookies - CSS - HTML ", link:"https://backend-commerce-dev.onrender.com/"},
+    { id: 6, title: "PORTAFOLIO", img:"../src/assets/img/portafolio.png", description:" Firebase - SAAS - CSS - HTML ", link:"https://lkwportafolio.netlify.app/"}
 
   ];
   
