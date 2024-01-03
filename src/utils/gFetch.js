373c57bb915a1ff028557600807c82cc6d06d3fd
Lucila Werner <lulikwerner@gmail.com>
2024-01-02 21:18:53 -0500

    pics

--- src/utils/gFetch.js
+++ src/utils/gFetch.js
@@ -3,7 +3,7 @@ let projects = [
     { id: 2, title: "BMI", img:"../src/assets/img/scale.png", description:"Javascript - CSS - HTML", link:"https://bodymass-calculator.netlify.app/" },
     { id: 3, title: "MIA JEWELERS", img:"../src/assets/img/jewelry2.png", description:"Javascript - React - CSS - HTML", link:"https://miajewelers.netlify.app/" },
     { id: 4, title: "OYL FASHION", img:"/assets/img/OYL.png", description:"Javascript - React - CSS - HTML - Figma", link:"https://oyl-indumentaria.netlify.app/"},
-    { id: 5, title: "FULLSTACK", img:"/src/assets/img/ecommerce.png", description:"Javascript - Mongoose - Postman - NPM - Token - Cookies - CSS - HTML - Sweet Alert", link:"https://backend-commerce-dev.onrender.com/"},
+    { id: 5, title: "FULLSTACK", img:"./src/assets/img/ecommerce.png", description:"Javascript - Mongoose - Postman - NPM - Token - Cookies - CSS - HTML - Sweet Alert", link:"https://backend-commerce-dev.onrender.com/"},
     { id: 6, title: "PORTAFOLIO", img:"../../src/assests/ims/ecommerce.png", description:" Firebase - SAAS - CSS - HTML - Boostrap ", link:"https://lkwportafolio.netlify.app/"}
 
   ];
